1c50462bcf6d73e3e9d375f974ef363da396de9d
yogaraj <yogaraj.t@localramu.com>
2025-12-28 21:09:53 +0530

    Fix GitHub Pages HashRouting routing

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 const routes: Routes = [
   {
     path: '',
@@ -19,6 +19,9 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [
+    { provide: LocationStrategy, useClass: HashLocationStrategy }
+  ]
 })
 export class AppRoutingModule { }
